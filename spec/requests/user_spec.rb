c5efc677b3be4afc2d0ad0507d768d56cc28c64c
wilma <wilminhasilva@gmail.com>
2022-09-12 21:55:55 -0300

    Testes do controller user

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -5,18 +5,49 @@ RSpec.describe "Users", type: :feature do
   before do
     sign_in user
   end
-  describe "GET /create" do
-    scenario "Quando acessar formulário do cadastro de livro" do
+  describe " /index" do
+    scenario "Acessando listagem de usuário" do
+     
+      visit '/users'
+
+      expect(page).to have_content('Usuários cadastrados')
+      expect(page).to have_content('Novo usuário')
+    end
+
+    scenario "Quando clicar no botão cadastrar usuário" do
+
+      visit '/users'
+
+      click_link('Novo usuário')
+      expect(page).to have_content('Cadastro de usuário')
+    end
+  end
+  
+  describe " /new" do
+    scenario "Quando for cadastrar usuário" do
 
       visit "/users/new"
      
-      # fill_in "user[name]", with: 'Wilma'
-      # fill_in "user[email]", with: 'teste@teste.com'
-      # fill_in "user[permission]", with: 1
+      fill_in "user[email]", with: 'teste@teste.com'
+      select(user.permission.humanize, from: 'user[permission]')
+      click_button('Salvar')
       expect(page).to have_content('Usuário cadastrado')
-    
-      # click_button('Salvar')
-      
+   
+    end
+  end
+
+  describe " /edit" do
+    scenario "Quando editar usuário " do
+      user = User.create(name:"Silva", email: "silva@gmail.com", password:"123", permission:"common")
+      params = { user: {
+        email: "wsilva@hotmail.com"
+        }
+      }
+
+      visit "/users/#{user.id}/edit"
+
+      click_button('Salvar')
+      expect(page).to have_content('Usuário atualizado!')
     end
   end
 end
